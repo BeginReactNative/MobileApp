18b1aa155c129b9d2ed3876eb9666d1d4564244b
BeginReactNative <nguyenphuctrungkien@gmail.com>
2017-07-07 22:28:26 +0700

    Design Exercise

--- src/screens/authorized/courses/lesson_tab/Exercise.js
+++ src/screens/authorized/courses/lesson_tab/Exercise.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-import { View,Text,WebView,Dimensions } from 'react-native';
+import { View,Text,WebView,Dimensions,FlatList } from 'react-native';
 import {connect} from 'react-redux';
 import Swiper from 'react-native-swiper';
 import Carousel from 'react-native-snap-carousel';
+import CardExercise from '../../../../components/CardExercise';
 const {width, height} = Dimensions.get('window')
 class Exercise extends Component {
-  
+     _renderItem = ({ item }) => (
+            <CardExercise 
+                frontText={item.frontText}
+                multiChoices={item.multiChoices}
+                backText={item.backText}
+                index={item.index}
+            />
+    );
     render() {
         return (
             <View style={styles.container}>
@@ -24,14 +32,21 @@ class Exercise extends Component {
                    this.props.cards.map((card,i) =>(
                        
                         <View style={{ flex:1,borderWidth:1, borderRadius: 5,width: width}} key={i}>
-                        <Text>{i}</Text>
+                        <Text>Cau {i +1 }</Text>
                         <WebView style={styles.web} 
                             source={{html: card.frontText}}
                         >
                         </WebView>
-                        <View style={{height: height * 0.6, borderWidth:1}}>
-                            <Text>AAA</Text>
-                        </View>
+                       <View style={{height: 400}}>
+                            <FlatList
+                                    data={card.childCards}
+                                    extraData={this.state}
+                                    keyExtractor={item => item.id}
+                                    renderItem={this._renderItem}
+                            />
+                        
+                       </View>
+                    
                        </View>
                        
                    ))
@@ -44,7 +59,8 @@ class Exercise extends Component {
 const mapStateToProps = (state) => {
     return {
         cards: state.cardById.data,
-        isFetching: state.cardById.isFetching
+        isFetching: state.cardById.isFetching,
+        
     }
 }
 export default connect(mapStateToProps)(Exercise);
@@ -54,8 +70,8 @@ export default connect(mapStateToProps)(Exercise);
      },
      web: {
          margin: 10,
-        
-         borderRadius: 5,
+         borderRadius:10,
+         
          backgroundColor: '#f4f7f9'
 
      }
