b4ad3878eab3001ca6c4a0c6eaa62fc0b98d0a8c
BeginReactNative <nguyenphuctrungkien@gmail.com>
2017-07-13 21:10:39 +0700

    Add Intro App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import { StyleProvider,Container } from 'native-base';
+import { StyleProvider, Container } from 'native-base';
 import { Provider } from 'react-redux';
 import store from './configureStore';
-
 import TestCom from './TestComponent';
 import RootNav from './navigations/RootNav';
 import getTheme from '../native-base-theme/components';
@@ -11,13 +10,11 @@ class App extends Component {
     render() {
         return (
             <Provider store={store}>
-            
-            <StyleProvider style={getTheme(material)}>
-                <Container>
-                    <RootNav />
-                </Container>
-                
-            </StyleProvider>
+                <StyleProvider style={getTheme(material)}>
+                    <Container>
+                        <RootNav />
+                    </Container>
+                </StyleProvider>
             </Provider>
         );
     }
--- src/screens/unauthorized/Login.js
+++ src/screens/unauthorized/Login.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
-import { View, Text,TouchableOpacity } from 'react-native';
-
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import AppIntro from 'react-native-app-intro';
+import IntroSCR from '../../components/IntroScreen';
 class Login extends Component {
     render() {
         return (
-            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                <TouchableOpacity onPress={() => this.props.navigation.navigate('SignInSCR')}>
-                <Text> Login Screen</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => this.props.navigation.navigate('Authorized')}>
-                <Text> Home Screen</Text>
-                </TouchableOpacity>
-            </View>            
+            <AppIntro 
+                onSkipBtnClick={(index) => this.props.navigation.navigate('Authorized')}
+                onDoneBtnClick={(index) => this.props.navigation.navigate('Authorized')}
+            >
+              
+                    <IntroSCR 
+                        image={'https://hola-edu.appspot.com/resources/images/home/banner1.jpg'}
+                        title={'Kiểm tra đầu vào'}
+                        desc1={'Take the Diagnotic Test -- NOW!'}
+                        desc2={'What you need!'}
+                    />
+                    <IntroSCR image={'https://hola-edu.appspot.com/resources/images/home/banner2.png'}/>
+            </AppIntro>
         );
     }
 }
-
+const styles = StyleSheet.create({
+    slide: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#9DD6EB',
+        padding: 15,
+    },
+   
+});
 export default Login;
+
+
+    // <TouchableOpacity onPress={() => this.props.navigation.navigate('Authorized')}>
+    //             <Text> Home Screen</Text>
+    //             </TouchableOpacity>
